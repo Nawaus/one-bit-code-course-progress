56b1111758cf1ae44e626290110eea065ea26c51
Gustavo edson <ggugu9289@gmail.com>
2023-02-23 20:46:59 -0300

    update

---
+++ dom/practicing-dom/index.js
@@ -0,0 +1,44 @@
+function addContact() {
+  const contactSection = document.getElementById("contacts-list");
+  const h3 = document.createElement("h3");
+  h3.textContent = "Contato";
+  contactSection.append(h3);
+  const ul = document.createElement("ul");
+
+  const nameInput = document.createElement("input");
+  const nameLabel = document.createElement("label");
+  nameLabel.innerHTML = `<label for="name">Nome: <label>`;
+  nameInput.type = "text";
+  nameInput.name = "name";
+  nameInput.id = "name";
+  ul.append(nameLabel, nameInput, document.createElement("br"));
+  contactSection.append(ul);
+
+  const inputNumber = document.createElement("input");
+  const numberLabel = document.createElement("label");
+  numberLabel.innerHTML = `<label for="number">Telefone: <label>`;
+  inputNumber.type = "number";
+  inputNumber.name = "number";
+  inputNumber.id = "number";
+  ul.append(numberLabel, inputNumber, document.createElement("br"));
+  contactSection.append(ul);
+
+  const inputAdress = document.createElement("input");
+  const addresLabel = document.createElement("label");
+  addresLabel.innerHTML = `<label for="address">Endereço: <label>`;
+  inputAdress.type = "text";
+  inputAdress.name = "addres";
+  inputAdress.id = "number";
+  ul.append(addresLabel, inputAdress, document.createElement("br"));
+  contactSection.append(ul);
+}
+
+function removeContact() {
+  const contactSection = document.getElementById("contacts-list");
+
+  const titles = document.getElementsByTagName("h3");
+  const contacts = document.getElementsByTagName("ul");
+
+  contactSection.removeChild(titles[titles.length - 1]);
+  contactSection.removeChild(contacts[contacts.length - 1]);
+}
