bc977e8e3a6e49b723298e6e7d62f7edfbcbdab3
Gustavo edson <ggugu9289@gmail.com>
2023-02-22 23:01:17 -0300

    update

---
+++ dom/events/index.js
@@ -0,0 +1,19 @@
+const showMouse = () => {
+  console.log("Entrada do mouse");
+};
+
+const sayHi = () => {
+  console.log("ola");
+};
+
+// const onfocus = () => {
+//   console.log("yo");
+// };
+
+// const onblur = () => {
+//   console.log("...");
+// };
+
+function click() {
+  console.log("Click");
+}
