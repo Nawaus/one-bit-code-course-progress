b76076d306e0be35133b1be9e9a539c21cad25d3
Gustavo edson <ggugu9289@gmail.com>
2023-03-05 12:14:41 -0300

    update

--- dom/dev_registration/index.js
+++ dom/dev_registration/index.js
@@ -1,7 +1,43 @@
 function registerDevs() {
   const form = document.querySelector("form");
-  form.addEventListener("submit", (e) => {
+  const button = document.querySelector(".add");
+  button.addEventListener("click", (e) => {
     e.preventDefault();
+
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const h3 = document.createElement("h3");
+    h3.textContent = "tempo de experiência";
+    const inputRadios = document.createElement("input");
+    inputRadios.type = "radio";
+
+    label.htmlFor = "tec";
+    label.textContent = `Nome da tecnologia`;
+    input.id = "tec";
+    input.type = "text";
+    input.name = "tec";
+    const div = document.createElement("div");
+    div.class = "content";
+
+    const button = document.createElement("button");
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      div.remove();
+    });
+
+    button.id = "remove";
+    button.textContent = "Remover essa linha";
+    div.append(
+      h3,
+      inputRadios,
+      document.createElement("br"),
+      label,
+      input,
+      button,
+      document.createElement("br"),
+      document.createElement("br")
+    );
+    form.append(div);
   });
 }
 registerDevs();
